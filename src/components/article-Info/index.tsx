6c857d61ae0c9bbefb9a8ad8d3149a57c938786b
Emmanuel Lopez <emmanuel.lopez@meltsan.com>
2025-10-21 12:07:22 -0600

    feat(component): implement empty component to use in future

---
+++ src/components/article-Info/index.tsx
@@ -0,0 +1,18 @@
+
+
+type Props = {
+    author: string;
+    postDate: string;
+
+    readingTime: number;
+
+};
+
+const ArticleInfo = ({ author, postDate, readingTime }: Props) => {
+
+    return (
+        
+    )
+}
+
+export default ArticleInfo;
